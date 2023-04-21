b3c67a660a22056003d24f160fd20ddfb03bb2b7
Sven Becker <sven.becker@epfl.ch>
2023-04-21 18:22:15 +0200

    Use arti-odom

--- config/cartographer.lua
+++ config/cartographer.lua
@@ -21,12 +21,12 @@ options = {
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
   tracking_frame = "imu_sensor_link",
-  published_frame = "base_link",
+  published_frame = "odom",
   odom_frame = "odom",
-  provide_odom_frame = true,
+  provide_odom_frame = false,
   publish_frame_projected_to_2d = true,
   use_pose_extrapolator = false,
-  use_odometry = false,
+  use_odometry = true,
   use_nav_sat = false,
   use_landmarks = false,
   num_laser_scans = 1,
@@ -34,9 +34,9 @@ options = {
   num_subdivisions_per_laser_scan = 1,
   num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.2,
-  submap_publish_period_sec = 0.3,
+  submap_publish_period_sec = 1,
   pose_publish_period_sec = 5e-3,
-  trajectory_publish_period_sec = 30e-3,
+  trajectory_publish_period_sec = 10,
   rangefinder_sampling_ratio = 1.,
   odometry_sampling_ratio = 1.,
   fixed_frame_pose_sampling_ratio = 1.,
@@ -47,19 +47,21 @@ options = {
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
-MAP_BUILDER.num_background_threads = 2
+MAP_BUILDER.num_background_threads = 4
 TRAJECTORY_BUILDER_2D.use_imu_data = true
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.num_threads = 2
-POSE_GRAPH.optimize_every_n_nodes = 1
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.num_threads = 4
+POSE_GRAPH.optimize_every_n_nodes = 10
+TRAJECTORY_BUILDER_2D.min_range = 0.5
+TRAJECTORY_BUILDER_2D.max_range = 15.0
 
--- TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 5e0
--- TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 2e1
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 5e0
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 2e-2
 
 
 POSE_GRAPH.optimization_problem.local_slam_pose_translation_weight = 100
 POSE_GRAPH.optimization_problem.local_slam_pose_rotation_weight = 100
-POSE_GRAPH.optimization_problem.odometry_translation_weight = 5
-POSE_GRAPH.optimization_problem.odometry_rotation_weight = 5
+POSE_GRAPH.optimization_problem.odometry_translation_weight = 10
+POSE_GRAPH.optimization_problem.odometry_rotation_weight = 10 
 
 
 return options
